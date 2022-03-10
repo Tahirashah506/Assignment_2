7391032f4b48ceeae23dad9e957468fd85d85b92
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-10 21:32:13 +0500

    Add files via upload

---
+++ practicework.dart
@@ -0,0 +1,99 @@
+void main(){
+ //stdout.write("user name");
+ //var a=stdin.readLineSync();
+ //stdout.write("quantity");
+ //int b= int.parse(stdin.readLineSync()!);
+ //stdout.write("product name");
+ //var c=stdin.readLineSync();
+ //var a1=150;
+ //var stqty=5;
+ //var a2=300;
+ //var sqty=7;
+ //var a3=250;
+ //var bqty=4;
+ //if(c=="shirt"){
+ //if(b<=stqty){
+ //print("you get this item");
+ //var price = b*a1;
+ //print("price:${price}");
+ //}
+ //else{
+ //print("out of stock");
+ //}
+ //}
+ //else if(c=="shoes"){
+ //if(b<=sqty){
+ //print("you get this item");
+ //var price = b*a2;
+ //print("price:${price}");
+ //}
+ //else{
+ //print("out of stock");
+ //}
+ //}
+ //else if(c=="book"){
+ //if(b<bqty){
+ //print("you get this item");
+ //var price=b*a3;
+ //print("price:${price}");
+ //}
+ //else{
+ //print("out of stock");
+ //}
+ //}
+ //else{
+ //print("not available");
+ //}
+
+
+
+ //int a=int.parse(stdin.readLineSync()!);
+ //int b=int.parse(stdin.readLineSync()!);
+ //int c=int.parse(stdin.readLineSync()!);
+ //bool a1=a<b && a<c;
+ //bool b1=a<b || a<c;
+ //bool c1=! (a<b);
+ //bool d1=!(a<c);
+ //print(a1);
+ //print(b1);
+ //print(c1);
+ //print(d1);
+
+
+
+
+//String a="TAHIRA shah";
+ //print(a.toUpperCase());
+ //print(a.toLowerCase());
+ //print(a.indexOf("s"));
+ //print(a.indexOf("z"));
+ //print(a.lastIndexOf("h"));
+ //print(a.indexOf("h"));
+ //print(a.indexOf("H"));
+ //print(a.codeUnits);
+ //print(a.codeUnitAt(3));
+ //if(a.codeUnitAt(0)>=65 && a.codeUnitAt(0)<=90 && a.codeUnitAt(1)>=65 && a.codeUnitAt(1)<=90){
+ // print("capital letter");
+ //}
+ //else{
+ //print("plz enter must capital index ate 0 or 1");
+ //}
+ //print(a.contains("R"));
+ //print(a.startsWith("s"));
+ //print(a.endsWith("I"));
+ //print(a.replaceAll("taira","tahira"));
+ //print(a.replaceFirst("h", "i"));
+ //print(a.split(" "));
+ //var name=a.split(" ");
+ //print("tahira");
+
+
+
+ //int num1 = int.parse(stdin.readLineSync()!);
+//int num2 = int.parse(stdin.readLineSync()!);
+//var op = stdin.readLineSync();
+// + => concatenation , $=>interploation
+ //if(op=="+"){
+   //print("value of A: ${num1} \n value of B; \n sum of A+B : ${num1+num2}");
+ //}
+}
