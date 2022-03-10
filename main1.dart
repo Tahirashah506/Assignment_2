ce6fc3c886fa3f7b205509c7332da8919c5b70e4
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-10 21:33:27 +0500

    Add files via upload

---
+++ main1.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void main(){
+  //stdout.write("vistor's name:");
+  //var name =stdin.readLineSync();
+  //stdout.write("product title:");
+  //var prod =stdin.readLineSync();
+  //stdout.write("quantity:");
+  //var qty =stdin.readLineSync();
+  //String a= "${name} ordered ${qty} ${prod} on baig mart";
+  //print(a); 
+  //int b=int.parse(stdin.readLineSync()!);
+  //int c=int.parse(stdin.readLineSync()!);
+  //int sum=b+c;
+  //print("sum of ${b} and sum of ${c} is ${sum}");
+  //int b1= int.parse(stdin.readLineSync()!);
+  //int c1= int.parse(stdin.readLineSync()!);
+  //int subtraction =b1-c1;
+  //print("subtraction of ${b1} and subtraction of ${c1} is :${subtraction}");
+  //int b2=int.parse(stdin.readLineSync()!);
+  //int c2=int.parse(stdin.readLineSync()!);
+  //int multi = b2*c2;
+  //print("multi of ${b2} and multi of ${c2} is :${multi}");
+  //int b3=int.parse(stdin.readLineSync()!);
+  //int c3=int.parse(stdin.readLineSync()!);
+  //double division=b3/c3;
+  //print("division of ${b3} and division of ${c3} is :${division}");
+  //int b4=int.parse(stdin.readLineSync()!);
+  //int c4=int.parse(stdin.readLineSync()!);
+  //int modulus=b4%c4;
+  //print("modulus of ${b4} and modulus of ${c4} is :${modulus}");
+  //var ticketprice=1000;
+  //stdout.write("user input:");
+  //int user=int.parse(stdin.readLineSync()!);
+  //var multi=ticketprice*user;
+  //print("${ticketprice} and ${user} is :${multi}");
+  //stdout.write("celsius:");
+  //int degree=int.parse(stdin.readLineSync()!);
+  //double a=degree*9;
+  //double b=a/5;
+  //double c=b+32;
+  //print("fahrenheit of ${degree} and celsius is ${c}");
+  //int qtyshoes=8;
+  //int qtybags=5;
+  //print("i have 8 shoes and 5 bags");
+  //int shoes=800;
+  //int bags=500;
+  //stdout.write("how much shoes you want:");
+  //int shoe=int.parse(stdin.readLineSync()!);
+  //stdout.write("how much bags you want:");
+  //int bag=int.parse(stdin.readLineSync()!);
+  //int shoecost=shoes*shoe;
+  //int bagcost=bags*bag;
+  //print("your total cost of shoes is ${shoecost}");
+  //print("your total cost of bags is ${bagcost}");
+  //int total=800;
+  //print("total marks:${total}");
+  //stdout.write("obtained marks:");
+  //int obtained=int.parse(stdin.readLineSync()!);
+  //num reminder=obtained/total*500;
+  //print("percentage=${reminder}");
+  //int a1=10;
+  //int b1=25;
+  //print("exchange rates");
+  //var a=175.70;
+  //var b=46.85;
+  //var get_dollar = a1*a;
+  //var get_rail = b1*b;
+  //var total = get_dollar+get_rail;
+  //print("1 us dollar= ${a} pakistani rupee");
+  //print("10 us dollar= ${get_dollar} pakistani rupee");
+  //print("1 saudi rail= ${b} pakistani rupee");
+  //print("25 saudi rail=${get_rail} pakistani rupee");
+  //print("total currancy to pakistani rupee= ${total}");
+  //stdout.write("enter hours:");
+  //int hours= int.parse(stdin.readLineSync()!);
+  //int a=400;
+  //print("enter rate per hours: ${a}");
+  //var total=hours*a;
+  //print("your weekly earning is: ${total}");
+
+
+
+ //print("\t\t\t\t\tcovid-19 vaccination card");
+ //print("Vaccination center:\t\tkotri.");
+ //print("registration no \t\t\t 5268");
+ //stdout.write("name:");
+ //stdin.readLineSync();
+ //stdout.write("cnic no:");
+ //stdin.readLineSync();
+ //stdout.write("contact no:");
+ //stdin.readLineSync();
+
+
+
+//  int a = Random().nextInt(10);
+//  print(a);
+//  var b=Random().nextDouble();
+//  print(b);
+//  var c=Random().nextBool();
+//  print(c);
+//  int rnum=Random().nextInt(10);
+//  print(rnum);
+//  stdout.write("enter any num");
+//  int c=int.parse(stdin.readLineSync()!);
+//  if(rnum<c){
+//    print("greater and incorrect");
+//  } 
+//  else if(rnum<c){
+//    print("less and incorrect");
+//  }
+//  else if(rnum<c){
+//    print("equal and great");
+//  }
+}
