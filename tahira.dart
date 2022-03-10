98e7c0286c9d307bc946c5188aa59541b5c3a5b3
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-10 20:18:01 +0500

    Add files via upload

---
+++ tahira.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+void main(){
+  //stdout.write("enter user city name:");
+  //{
+  //var a=stdin.readLineSync()!.toLowerCase();
+  //if(a=="karachi"){
+  //print("welcome to city of karachi");
+  //}
+  //else if(a=="multan"){
+  //print("no seat available");
+  //}
+  //}
+  //else if(a=="islamabad"){
+  //print("no seat available");
+  //}
+  //stdout.write("enter your gender:");
+  //var b=stdin.readLineSync()!.toUpperCase();
+  //{
+  //if(b=="MALE"){
+  //print("GOOD MONING SIR");
+  //}
+  //else if(b=="FEMALE"){
+  //print("GOOD MORNING MADAM");
+  //}
+  //}
+  //stdout.write("enter user temperature:");
+  //int tem=int.parse(stdin.readLineSync()!);
+//{
+  //if(tem>40){
+    //print("then it is too hot outsid.");
+  //}
+  //else if(tem>30){
+    //print("then the weather tody is normal.");
+  //}
+  //else if(tem>20){
+    //print("then today weather is cool.");
+  //}
+  //else if(tem>10){
+    //print("then OMG! Today weather is so cool.");
+  //}
+//}
+//stdout.write("enter value 1:");
+//int a=int.parse(stdin.readLineSync()!);
+//stdout.write("enter value 2:");
+//int b=int.parse(stdin.readLineSync()!);
+//if(a>b){
+  //print("A is greater");
+  //print("B is small");
+//}
+//else if(b>a){
+  //print("B is greater");
+  //print("A is small");
+//}
+//else if(a==b){
+  //print("A and B is equal");
+//}
+//stdout.write("color of road traffic signal:");
+//var a1=stdin.readLineSync();
+//if(a1=="red"){
+  //print("red must stop");
+//}
+//else if(a1=="yellow"){
+  //print("yellow ready to move");
+//}
+//else if(a1=="green"){
+  //print("green move now");
+//}
+}
