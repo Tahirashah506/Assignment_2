71973918bfa3ea21b8f84ff81405dc5cdf788dc5
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-12 12:37:30 +0500

    Add files via upload

---
+++ tahirashah.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+void main(){
+  //Q1
+  //List str = ["tahira","shah","zabtech","hyderabad","islamabad"];
+  //print(str);
+  //Q2
+  //List array = ["tahira syed",13,true,false,20.1,48];
+  //print(array);
+  //Q3
+  // List education = ["ssc","hsc","bsc","bs","bcom","ms","m.phill","phd"];
+  // print(education[0]);
+  // print(education[1]);
+  // print(education[2]);
+  // print(education[3]);
+  // print(education[4]);
+  // print(education[5]);
+  // print(education[6]);
+  // print(education[7]);
+  //Q4
+  // List stu_name = ["iqra","tahira","linta noor"];
+  // print("student name: ${stu_name}");
+  // List score = [450,420,370];
+  // print("total marks=500");
+  // print("linta noor obtained marks: ${score[0]}");
+  // double per = score[0] / 500 * 100;
+  // print("percentage=${per}");
+  // print("total marks=500");
+  // print("tahira obtained marks: ${score[1]}");
+  // double per1 = score[1] / 500 * 100;
+  // print("percentage=${per1}");
+  // print("total marks=500");
+  // print("iqra obtained marks: ${score[2]}");
+  // double per2 = score[2] / 500 * 100;
+  // print("percentages= ${per2}");
+  //Q5
+  // List score_total = [450,420,370,200,500];
+  // score_total.sort();
+  // print("student total score in ascending order:");
+  // print(score_total);
+  //Q6
+  // List score_total1 = [400,420,370,200,500];
+  // score_total1.sort();
+  // print("student total score in decending order:");
+  // print(score_total1.reversed.toList());
+  //Q7
+  // List pro_lang = ["android","beauticion","electricion","fashion designing","php"];
+  // var pro_lang1 = "AI";
+  // var pro_lang2 = "artifical";
+  // if(pro_lang.length > 3) {
+  //   pro_lang.replaceRange(2, 3, [pro_lang1, pro_lang2]);
+  //   print(pro_lang);
+  // } else {
+  //   print("enter correct data");
+  // }
+  //Q8
+  // List color = ["blue","pink","red","black","purple"];
+  //print("colour name:${color}");
+  //a
+  // stdout.write("what color he/she wants to add to the begining:");
+  // var color1 = stdin.readLineSync();
+  // color[0] = color1;
+  // print(color);
+  //b
+  // stdout.write("what color he/she wants to add to the ending:");
+  // var color2 = stdin.readLineSync();
+  //color.add(color2);
+  // print(color);
+  //c
+  // print("add to more color to the begining");
+  // stdout.write("what color he/she wants to add to the begining:");
+  // var color3 = stdin.readLineSync();
+  // stdout.write("add two color to the begining:");
+  // var color4 = stdin.readLineSync();
+  // color.insertAll(0,[color3,color4]);
+  // print(color);
+  //d
+  // color.removeAt(0);
+  // print(color);
+  // color.insert(0,"black");
+  // print(color);
+  //e
+  // color.removeLast();
+  // print(color);
+  // color.insert(4,"pink");
+  // print(color);
+  //f
+  // stdout.write("which index he/she wants to remove:");
+  // int ind = int.parse(stdin.readLineSync()!);
+  // stdout.write("which color add:");
+  // var color5 = stdin.readLineSync();
+  // color.insertAll(ind,[color5]);
+  // print(color);
+  //g
+  //print("first element:${color.first}");
+  //print("last element:${color.last}");
+  //Q9
+  // print("cities array:");
+  // List city = ["karachi","hyderabad","sargodha","islamabad"];
+  // print(city);
+  // print("selected cities:");
+  // print(city.sublist(1,3));
+  //Q10 
+  // var arr = ["this","is","a","watch"];
+  // print(arr.join());
+  // print("this is my string"); 
+  //Q11      
+  //  stdout.write("enter password:");
+  //  var pas = stdin.readLineSync()!;
+  //  if(pas.codeUnitAt(0) >= 47 && pas.codeUnitAt(0) <= 57) {
+  //    print("not start with number");
+  //  }
+  //  else if (pas.length < 6) {
+  //    print("enter at least 6 characters long");
+  //  }
+  //  else if (pas.codeUnitAt(0) >= 97 && pas.codeUnitAt(0) <= 122) {
+  //    print("enter capital letter");
+  //  }
+  }
