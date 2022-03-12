1d695cf886fea39245d33228bf9023d3389ed5bf
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-12 12:43:48 +0500

    Add files via upload

---
+++ syeda.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+void main(){
+  //Q1
+  //var a = "dart course";
+  //var b = a.indexOf(" ");
+  //var res = a[0]. toUpperCase() + a.substring(1, b + 1).toLowerCase() + a[b+1].toUpperCase() + a.substring(b+2).toLowerCase();
+  //print(res);
+  //print(a.toUpperCase());
+  //print(a.toLowerCase());
+  //Q2
+  //var a1 = 2;
+  //var b1 = 1;
+  //var result = (--a1) - (--b1) + (++b1) + (b1--);
+  //--a1=1 
+  //--a1---b1=1 
+  //--a1---b1+++b1=2 
+  //--a1---b1+++b1+b1--=3 
+  //print(result);
+  //Q3
+  //String c = "Tahira";
+  //String d = "Tahira";
+  //print(c.compareTo(d));
+  //String d1 = "Tahira";
+  //print(c.compareTo(d1));
+  //String d2 = "Tahira";
+  //print(c.compareTo(d2));
+  //Q4
+  //stdout.write("user enter name:");
+  //String c1=stdin.readLineSync()!;
+  //print("length: ${c1.length}");
+  //Q5
+  //String e = "Tahira560";
+  //print("Ascii code: ${e.codeUnits}");
+  //Q6
+  //stdout.write("user enter any character:");
+  //String e1 = stdin.readLineSync()!;
+  //if (e1.length>7){
+  //print(e1.substring(4,7));
+  //} 
+  //else{
+  //print("enter long characters");
+  //}
+  //Q7
+  //String e2 = "zebtech";
+  //print(e2.split("e"));
+}
