dddba658be9d1c72b10ffc099c586f4cc88ca6b8
Tahirashah506 <98934865+Tahirashah506@users.noreply.github.com>
2022-03-12 12:33:17 +0500

    Add files via upload

---
+++ chance.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  // var i = "y";
+  // int chance = 3;
+  // int point = 0;
+  // while(i == "y"){
+  //   int b = Random().nextInt(10);
+  //   print(b);
+  //   stdout.write("enter number:");
+  //   int user = int.parse(stdin.readLineSync()!);
+
+  //   if(chance == 3){
+  //     if(b == user){
+  //       point += 30;
+
+  //       print("chance: ${chance}");
+  //       print("point: $point");
+  //       if(point >= 200){
+  //         print("you win game and exit");
+  //         i = "y";
+  //       }
+  //     } else{
+  //       chance--;
+  //       print("chance: $chance");
+  //       print("point: $point");
+  //     }
+  //   } else if (chance == 2){
+  //     if (b == user){
+  //       point += 20;
+  //       chance++;
+
+  //      print("chance: ${chance}");
+  //      print("point: $point");
+  //      if(point >= 200){
+  //         print("you win game and exit");
+  //         i = "y";
+  //      }
+  //     } else{
+  //       chance--;
+  //       print("chance: $chance");
+  //       print("point: $point");
+  //     }
+  //   } else if (chance == 1){
+  //     if (b == user){
+  //       point += 10;
+  //       chance++;
+      
+  //     print("chance: ${chance}");
+  //      print("point: $point");
+  //      if(point >= 200){
+  //         print("you win game and exit");
+  //         i = "y";
+  //      }
+  //     } else{
+  //       chance--;
+  //       print("chance: $chance");
+  //       print("point: $point");
+  //     }
+  //   }
+  //   if (chance == 0) {
+  //     i = "n";
+  //   }
+  // }
+}
